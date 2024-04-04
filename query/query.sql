cd923e44b55ad8cc43446f1b56fdd2945e4a095b
Fauziah0121 <zyah92832@gmail.com>
2024-04-04 12:59:48 +0700

    Initial commit

---
+++ query/query.sql
@@ -0,0 +1,17 @@
+select	order_id,
+		order_date,
+		a.user_id,
+		b.payment_name,
+		c.shipper_name,
+		order_price,
+		order_discount,
+		d.voucher_name,
+		d.voucher_price,
+		order_total,
+		e.rating_status
+from zyah_data_raw.tb_orders a
+	left join zyah_data_raw.tb_payments b on a.payment_id = b.payment_id 
+	left join zyah_data_raw.tb_shippers c on a.shipper_id = c.shipper_id 
+	left join zyah_data_raw.tb_vouchers d on a.voucher_id = d.voucher_id 
+	left join zyah_data_raw.tb_ratings e on a.rating_id = e.rating_id ;
+	
